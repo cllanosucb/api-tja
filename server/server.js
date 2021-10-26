8558f32ddeec52d6bb84958c7a10b2c0e28e669c
cllanosucb <cllanos@ucb.edu.bo>
2021-10-26 12:04:40 -0400

    Inicio de API

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('./config/config')
+const express = require('express')
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+// parse application/json
+app.use(express.json())
+
+app.get('/', function (req, res) {
+  res.json('Hello World')
+})
+ 
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", process.env.PORT);
+})
