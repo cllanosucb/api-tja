b0884aa949299ab8bb5e18ff248d8f7af68a6d43
cllanosucb <cllanos@ucb.edu.bo>
2021-11-15 12:06:48 -0400

    log

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ const app = express()
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
-// parse application/json
+    // parse application/json
 app.use(express.json())
 
 //Configuracion global de rutas
 app.use('/api', require('./routes/index'))
- 
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: "Servidor iniciado"
+    });
+})
+
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto: ", process.env.PORT);
 })
